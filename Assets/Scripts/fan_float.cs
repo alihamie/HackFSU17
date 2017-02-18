75b37398e2ac96a1532c91f763c3959d8896bf2b
jamespar <jamespar@bu.edu>
2017-02-18 07:42:58 -0500

    add ramp & fan

---
+++ Assets/Scripts/fan_float.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fan_float : MonoBehaviour {
+    public int fan_force;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    // Applies an upwards force to all rigidbodies that enter the trigger.
+    void OnTriggerStay(Collider other) {
+        if (other.attachedRigidbody)
+            other.attachedRigidbody.AddForce(Vector3.up * fan_force);
+    }
+}
