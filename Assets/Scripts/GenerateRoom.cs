ad0a8873046269c967e8411d10f8ca92c2a53d97
alihamie <ali_leb29@hotmail.com>
2017-02-18 07:07:43 -0500

    First commit

---
+++ Assets/Scripts/GenerateRoom.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateRoom : MonoBehaviour {
+
+    public GameObject tilePrefab;
+
+    // public string url = "http://www.google.com";
+    public string url = "http://images.earthcam.com/ec_metros/ourcams/fridays.jpg";
+    void Start() {
+
+        StartCoroutine(getWeb());
+    }
+
+    IEnumerator getWeb()
+    {
+        WWW www = new WWW(url);
+        yield return www;
+        Renderer renderer = GetComponent<Renderer>();
+        renderer.material.mainTexture = www.texture;
+    }
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/TrampolineEffect.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrampolineEffect : MonoBehaviour {
+
+    public float bounceFactor = 100f;
+    void OnCollisionEnter(Collision collision)
+    {
+        collision.gameObject.GetComponent<Rigidbody>().AddRelativeForce(Vector3.up * bounceFactor);
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
